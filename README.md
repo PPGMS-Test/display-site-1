6a09444cb0fe3f29b37eed84328acf7afbd177c7
user-q123 <m605532833@163.com>
2023-10-22 23:53:31 +0800

    最后的调整

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-编辑readme文件
+使用`npm install`命令安装依赖  
+使用`npm start` 命令启动
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React, { FC } from "react";
 import "./App.css";
 import { Link } from "react-router-dom";
+import { Tooltip } from "@mui/material";
 
 const App: FC = () => {
     return (
         <div className=" m-auto">
             <div className=" w-auto px-2 m-4">
-                <button className="w-full bg-yellow-400 text-gray-800 py-2 px-4 rounded-full font-bold hover:bg-gray-300">
-                    <Link to="/display">Go To Display</Link>
-                </button>
+                <Tooltip
+                    title="For Partner, external use"
+                    placement="top-start"
+                >
+                    <button className="w-full bg-yellow-400 text-gray-800 py-2 px-4 rounded-full font-bold hover:bg-gray-300">
+                        <Link to="/display">Go To Display</Link>
+                    </button>
+                </Tooltip>
             </div>
             <div className="w-auto px-2 m-4">
-                <button className="w-full bg-blue-400 text-gray-800 py-2 px-4 rounded-full font-bold hover:bg-gray-300">
-                    <Link to="/lab">Go To Lab</Link>
-                </button>
+                <Tooltip title="For internal use" placement="top-start">
+                    <button className="w-full bg-blue-400 text-gray-800 py-2 px-4 rounded-full font-bold hover:bg-gray-300">
+                        <Link to="/lab">Go To Lab</Link>
+                    </button>
+                </Tooltip>
             </div>
         </div>
     );
