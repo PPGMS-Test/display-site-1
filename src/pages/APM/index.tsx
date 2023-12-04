44887e6a4fad841beb8fe26dd61278c05c3fe11d
Yuncong Qiang (CW) <yqiang@paypal.com>
2023-12-04 15:40:07 +0800

    update 打算放到自己电脑上开发

--- src/pages/APM/index.tsx
+++ src/pages/APM/index.tsx
@@ -1,11 +1,52 @@
-import { FC } from "react";
+import { ToggleButtonGroup, ToggleButton } from "@mui/material";
+import { FC, useState } from "react";
 const APM: FC = () => {
-  return (
-    <div>
-      Hello (APM Page)
-      <p>APM Sample Page is under construction ... SLA: 3</p>
-    </div>
-  );
+    const [APMMethod, setAPMMethod] = useState("Bancontact");
+
+    const handleChange = (
+        event: React.MouseEvent<HTMLElement>,
+        newAPM: string
+    ) => {
+        setAPMMethod(newAPM);
+    };
+
+    const APMMethodList = [
+        "Bancontact",
+        "BLIK",
+        "eps",
+        "giropay",
+        "iDEAL",
+        "MyBank",
+        "Pay upon invoice",
+        "Przelewy24",
+        "SOFORT",
+    ];
+
+    const children = APMMethodList.map((APMMethod) => {
+        return (
+            <ToggleButton
+                value={APMMethod}
+                key={APMMethod}
+                //margin不手动为0话会被设为-1, 看起来很奇怪;加粗字体看起来更清楚些
+                style={{ margin: "0px", fontWeight: 700 }}
+            >
+                {APMMethod}
+            </ToggleButton>
+        );
+    });
+    return (
+        <div>
+            <ToggleButtonGroup
+                size="large"
+                color="primary"
+                value={APMMethod}
+                exclusive
+                onChange={handleChange}
+            >
+                {children}
+            </ToggleButtonGroup>
+        </div>
+    );
 };
 
 export default APM;
